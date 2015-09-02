275a9f9513be5171bd83b77876fa7208ef8e2628
Claudia Stefan <stefanclaudia71@yahoo.com>
2015-09-02 21:41:33 +0300

    Refactored code into model - view - octopus

--- app/js/main.js
+++ app/js/main.js
@@ -1,58 +1,113 @@
 (function () {
-    var cats = null,
-        currentSelectionElem = document.getElementById('current-selection'),
-        selectionNameElem = document.getElementById('selection-name'),
-        currentCountElem = document.getElementById('current-count'),
-        selectionPhotoElem = document.getElementById('selection-photo');
+    var model = {
+        init: function(afterInitCb) {
+            var that = this;
 
-    ajax.load('../cats.json', loadSuccessCb);
-    selectionPhotoElem.addEventListener('click', addClicks, false);
+            ajax.load('../cats.json', loadSuccessCb);
 
-    function loadSuccessCb (data) {
-        var parentElem = document.getElementById('selection-list'),
-            docFragment = document.createDocumentFragment(),
-            index = 0,
-            catsLength;
-        //We use a comparator to compare the name of two cats.
-        cats = data.sort(function (a, b) {
-            return a.name.localeCompare(b.name);
-        });
-        catsLength = cats.length;
-        console.log('We have loaded the cats:' + JSON.stringify(cats));
-        for (;index < catsLength; index += 1) {
-            var imgElem = document.createElement('img');
-            imgElem.id = cats[index].id;
-            imgElem.src = cats[index].photo;
-            imgElem.className = 'small-cat';
-            imgElem.addEventListener('click', selectCat, false);
-            docFragment.appendChild(imgElem);
+            function loadSuccessCb (data) {
+                //We use a comparator to compare the name of two cats.
+                that.cats = data.sort(function (a, b) {
+                    return a.name.localeCompare(b.name);
+                });
+
+                console.log('We have loaded the cats:' + JSON.stringify(that.cats));
+
+                if (afterInitCb) {
+                    afterInitCb();
+                }
+
+            }
+        },
+
+        getAllCats: function() {
+            return this.cats;
         }
-        parentElem.appendChild(docFragment);
-    }
+    };
 
-    function selectCat (event) {
-        var index = 0,
+
+    var octopus = {
+        getCats: function() {
+            return model.getAllCats();
+        },
+
+        selectCat: function (event) {
+            viewCurrentCat.init();
+            viewCurrentCat.render(event);
+        },
+
+        init: function() {
+            model.init(viewCatList.init);
+        }
+    };
+
+
+    var viewCatList = {
+        init: function() {
+            viewCatList.render();
+        },
+
+        render: function(){
+            var parentElem = document.getElementById('selection-list'),
+                docFragment = document.createDocumentFragment(),
+                index = 0,
+                catsLength,
+                cats = octopus.getCats();
+
             catsLength = cats.length;
-        currentSelectionElem.style.display = 'inline-block';
-        console.log('This cat was selected:' + event.target.id);
-        for (;index < catsLength; index += 1) {
-            if(cats[index].id === event.target.id) {
-                selectionNameElem.innerHTML = cats[index].name;
-                currentCountElem.innerHTML = cats[index].count;
-                selectionPhotoElem.src = cats[index].photo;
+
+            for (;index < catsLength; index += 1) {
+                var imgElem = document.createElement('img');
+                imgElem.id = cats[index].id;
+                imgElem.src = cats[index].photo;
+                imgElem.className = 'small-cat';
+                imgElem.addEventListener('click', selectCat, false);
+                docFragment.appendChild(imgElem);
+            }
+
+            function selectCat (event) {
+               octopus.selectCat(event);
+            }
+            parentElem.appendChild(docFragment);
+        }
+    };
+
+    var viewCurrentCat = {
+        init: function() {
+            var that = this;
+            this.currentSelectionElem = document.getElementById('current-selection');
+            this.selectionNameElem = document.getElementById('selection-name');
+            this.currentCountElem = document.getElementById('current-count');
+            this.selectionPhotoElem = document.getElementById('selection-photo');
+            this.selectionPhotoElem.addEventListener('click', addClicks, false);
+            function addClicks() {
+                var index = 0,
+                    cats = octopus.getCats(),
+                    catsLength = cats.length;
+
+                for (;index < catsLength; index += 1) {
+                    if(cats[index].name === that.selectionNameElem.innerHTML) {
+                        cats[index].count += 1;
+                        that.currentCountElem.innerHTML = cats[index].count;
+                    }
+                }
+            }
+        },
+
+        render: function (event){
+            var index = 0,
+                cats = octopus.getCats(),
+                catsLength = cats.length;
+            this.currentSelectionElem.style.display = 'inline-block';
+            console.log('This cat was selected:' + event.target.id);
+            for (;index < catsLength; index += 1) {
+                if(cats[index].id === event.target.id) {
+                    this.selectionNameElem.innerHTML = cats[index].name;
+                    this.currentCountElem.innerHTML = cats[index].count;
+                    this.selectionPhotoElem.src = cats[index].photo;
+                }
             }
         }
-    }
-
-    function addClicks() {
-        var index = 0,
-            catsLength = cats.length;
-
-        for (;index < catsLength; index += 1) {
-            if(cats[index].name === selectionNameElem.innerHTML) {
-                cats[index].count += 1;
-                currentCountElem.innerHTML = cats[index].count;
-            }
-        }
-    }
+    };
+    octopus.init();
 })();
